260dbd9b04cf7d4de1f74cccffdd28534f717b10
Paul Sho <Cyberkiller.CK@gmail.com>
2024-08-25 18:15:43 +0800

    created Happy Friends

--- app/page.js
+++ app/page.js
@@ -1,95 +1,208 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import { useEffect, useState } from "react";
+import "fastbootstrap/dist/css/fastbootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "../public/css/style.css";
 
 export default function Home() {
+  const [isSignUp, setIsSignUp] = useState(false);
+
+  const handleSignUpSwitch = () => {
+    setIsSignUp((prevIsSignUp) => !prevIsSignUp);
+  };
+
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div className="main">
+      {/* SIGN UP */}
+      {isSignUp ? (
+        <section className="signup">
+          <div className="container">
+            <div className="signup-content">
+              <div className="signup-form">
+                <h2 className="form-title">Sign up</h2>
+                <form
+                  method="POST"
+                  className="register-form"
+                  id="register-form"
+                >
+                  <div className="form-group">
+                    <label for="name">
+                      <i className="zmdi zmdi-account material-icons-name"></i>
+                    </label>
+                    <input
+                      type="text"
+                      name="name"
+                      id="name"
+                      placeholder="Your Name"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label for="email">
+                      <i className="zmdi zmdi-email"></i>
+                    </label>
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      placeholder="Your Email"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label for="pass">
+                      <i className="zmdi zmdi-lock"></i>
+                    </label>
+                    <input
+                      type="password"
+                      name="pass"
+                      id="pass"
+                      placeholder="Password"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label for="re-pass">
+                      <i className="zmdi zmdi-lock-outline"></i>
+                    </label>
+                    <input
+                      type="password"
+                      name="re_pass"
+                      id="re_pass"
+                      placeholder="Repeat your password"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="checkbox"
+                      name="agree-term"
+                      id="agree-term"
+                      className="agree-term"
+                    />
+                    <label for="agree-term" className="label-agree-term">
+                      <span>
+                        <span></span>
+                      </span>
+                      I agree all statements in{" "}
+                      <a href="#" className="term-service">
+                        Terms of service
+                      </a>
+                    </label>
+                  </div>
+                  <div className="form-group form-button">
+                    <input
+                      type="submit"
+                      name="signup"
+                      id="signup"
+                      className="form-submit"
+                      value="Register"
+                    />
+                  </div>
+                </form>
+              </div>
+              <div className="signup-image">
+                <figure>
+                  <img src="images/signup-image.jpg" alt="sing up image" />
+                </figure>
+                <a href="#" className="signup-image-link" onClick={handleSignUpSwitch}>
+                  I am already member
+                </a>
+              </div>
+            </div>
+          </div>
+        </section>
+      ) : (
+        <section className="sign-in">
+          {/* LOGIN */}
+          <div className="container">
+            <div className="signin-content">
+              <div className="signin-image">
+                <figure>
+                  <img src="/images/signin-image.jpg" alt="sing up image" />
+                </figure>
+                <a
+                  href="#signup"
+                  className="signup-image-link"
+                  onClick={handleSignUpSwitch}
+                >
+                  Create an account
+                </a>
+              </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+              <div className="signin-form">
+                <h2 className="form-title">Sign up</h2>
+                <form method="POST" className="register-form" id="login-form">
+                  <div className="form-group">
+                    <label for="your_name">
+                      <i className="zmdi zmdi-account material-icons-name"></i>
+                    </label>
+                    <input
+                      type="text"
+                      name="your_name"
+                      id="your_name"
+                      placeholder="Your Name"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label for="your_pass">
+                      <i className="zmdi zmdi-lock"></i>
+                    </label>
+                    <input
+                      type="password"
+                      name="your_pass"
+                      id="your_pass"
+                      placeholder="Password"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="checkbox"
+                      name="remember-me"
+                      id="remember-me"
+                      className="agree-term"
+                    />
+                    <label for="remember-me" className="label-agree-term">
+                      <span>
+                        <span></span>
+                      </span>
+                      Remember me
+                    </label>
+                  </div>
+                  <div className="form-group form-button">
+                    <input
+                      type="submit"
+                      name="signin"
+                      id="signin"
+                      className="form-submit"
+                      value="Log in"
+                    />
+                  </div>
+                </form>
+                <div className="social-login">
+                  <span className="social-label">Or login with</span>
+                  <ul className="socials">
+                    <li>
+                      <a href="#">
+                        <i className="display-flex-center zmdi zmdi-facebook"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <i className="display-flex-center zmdi zmdi-twitter"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <i className="display-flex-center zmdi zmdi-google"></i>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      )}
+    </div>
   );
 }
